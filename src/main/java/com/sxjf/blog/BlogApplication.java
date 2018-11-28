5a3b58d135cd355429655b2e3641c7429570419c
e <vo000o@163.com>
2018-11-28 23:28:18 +0800

    项目注解

--- src/main/java/com/sxjf/blog/BlogApplication.java
+++ src/main/java/com/sxjf/blog/BlogApplication.java
@@ -2,7 +2,13 @@ package com.sxjf.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * Created with IntelliJ IDEA.
+ * Description: Blog Project
+ * Author: wangyang
+ * Date: 2018/11/28
+ * Time: 23:27
+ */
 @SpringBootApplication
 public class BlogApplication {
 
